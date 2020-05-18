63bc1664d3168f27139563c1073425d74d2b5bfe
“伟” <1340597770@qq.com>
2020-05-18 11:30:53 +0800

    添加目录，修改首页

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '../src/page/Home.vue'
 
+
+//引入路由
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
