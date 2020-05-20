5e5d1c1050ffab1798c4c7ffb4077a021af6bddf
“”冯春炜“ <1197764821@qq.com>
2020-05-20 10:11:39 +0800

    Home

--- src/page/home.vue
+++ src/page/home.vue
@@ -1,17 +1,5 @@
 <template>
     <div>
-        <router-view></router-view>
+        
     </div>
 </template>
-<script>
-export default {
-    data() {
-        return {
-            
-        }
-    },
-}
-</script>
-<style lang="">
-    
-</style>
